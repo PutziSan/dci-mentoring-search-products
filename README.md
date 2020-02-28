615c6a9914225e6f7be3469792181b2d7f0e42df
Putzi San <putzisan@putzisan.de>
2020-02-28 09:35:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# List and Search Products
+
+In `index.js` is a faked product set of 100 products with the properties: `img_url`, `name`, `price`, `package_size` and `availability`.
---
+++ index.js
@@ -0,0 +1,725 @@
+/*
+{
+    "img_url": "https://lorempixel.com/640/480/technics/4",
+    "name": "Generic Plastic Shirt",
+    "package_size": "69 ml",
+    "price": 591,
+    "availability": "not_available"
+  }
+*/
+const products = [
+  {
+    img_url: "https://lorempixel.com/640/480/technics/4",
+    name: "Generic Plastic Shirt",
+    package_size: "69 ml",
+    price: 591,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/2",
+    name: "Unbranded Frozen Fish",
+    package_size: "76 ml",
+    price: 986,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/5",
+    name: "Rustic Fresh Cheese",
+    package_size: "19 mg",
+    price: 928,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/3",
+    name: "Intelligent Rubber Tuna",
+    package_size: "11 ml",
+    price: 643,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/4",
+    name: "Ergonomic Wooden Towels",
+    package_size: "38 mg",
+    price: 923,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/2",
+    name: "Generic Granite Sausages",
+    package_size: "2 mg",
+    price: 292,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/5",
+    name: "Sleek Plastic Ball",
+    package_size: "66 mg",
+    price: 922,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/sports/3",
+    name: "Practical Concrete Fish",
+    package_size: "72 ml",
+    price: 404,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/cats/8",
+    name: "Awesome Concrete Shoes",
+    package_size: "96 ml",
+    price: 745,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/6",
+    name: "Awesome Rubber Pants",
+    package_size: "80 ml",
+    price: 968,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/2",
+    name: "Generic Metal Keyboard",
+    package_size: "96 ml",
+    price: 276,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/5",
+    name: "Handmade Frozen Gloves",
+    package_size: "33 mg",
+    price: 555,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/transport/4",
+    name: "Generic Concrete Keyboard",
+    package_size: "73 mg",
+    price: 111,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/transport/1",
+    name: "Unbranded Cotton Car",
+    package_size: "16 mg",
+    price: 454,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/7",
+    name: "Licensed Cotton Salad",
+    package_size: "2 mg",
+    price: 189,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/cats/9",
+    name: "Generic Plastic Tuna",
+    package_size: "12 ml",
+    price: 879,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/5",
+    name: "Licensed Wooden Chicken",
+    package_size: "71 ml",
+    price: 927,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/2",
+    name: "Fantastic Wooden Shirt",
+    package_size: "45 mg",
+    price: 83,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/3",
+    name: "Sleek Soft Pants",
+    package_size: "22 ml",
+    price: 634,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/food/8",
+    name: "Unbranded Plastic Ball",
+    package_size: "15 ml",
+    price: 618,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/2",
+    name: "Refined Frozen Chips",
+    package_size: "20 ml",
+    price: 550,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/cats/3",
+    name: "Sleek Wooden Chicken",
+    package_size: "73 ml",
+    price: 165,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/9",
+    name: "Intelligent Wooden Tuna",
+    package_size: "88 mg",
+    price: 806,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/7",
+    name: "Generic Plastic Chips",
+    package_size: "39 mg",
+    price: 864,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/6",
+    name: "Licensed Rubber Fish",
+    package_size: "74 mg",
+    price: 321,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/4",
+    name: "Small Metal Hat",
+    package_size: "89 mg",
+    price: 806,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/4",
+    name: "Incredible Granite Keyboard",
+    package_size: "39 ml",
+    price: 745,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/4",
+    name: "Incredible Rubber Hat",
+    package_size: "52 ml",
+    price: 518,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/transport/8",
+    name: "Small Rubber Tuna",
+    package_size: "63 ml",
+    price: 635,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/transport/1",
+    name: "Practical Steel Fish",
+    package_size: "62 mg",
+    price: 327,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/people/8",
+    name: "Refined Concrete Car",
+    package_size: "98 mg",
+    price: 722,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/3",
+    name: "Tasty Plastic Shoes",
+    package_size: "77 ml",
+    price: 591,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/sports/8",
+    name: "Handcrafted Wooden Keyboard",
+    package_size: "75 mg",
+    price: 753,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/8",
+    name: "Handmade Rubber Mouse",
+    package_size: "93 mg",
+    price: 868,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/5",
+    name: "Sleek Soft Keyboard",
+    package_size: "89 ml",
+    price: 365,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/3",
+    name: "Unbranded Cotton Car",
+    package_size: "86 ml",
+    price: 350,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/6",
+    name: "Small Cotton Shirt",
+    package_size: "33 mg",
+    price: 376,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/people/1",
+    name: "Gorgeous Fresh Sausages",
+    package_size: "57 mg",
+    price: 352,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/3",
+    name: "Small Soft Shirt",
+    package_size: "41 ml",
+    price: 477,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/7",
+    name: "Gorgeous Metal Computer",
+    package_size: "20 ml",
+    price: 966,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/5",
+    name: "Rustic Steel Table",
+    package_size: "73 ml",
+    price: 588,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/5",
+    name: "Generic Wooden Keyboard",
+    package_size: "16 mg",
+    price: 333,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/8",
+    name: "Handmade Fresh Car",
+    package_size: "69 ml",
+    price: 993,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/8",
+    name: "Sleek Steel Ball",
+    package_size: "35 mg",
+    price: 307,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/3",
+    name: "Rustic Steel Bacon",
+    package_size: "96 mg",
+    price: 306,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/7",
+    name: "Gorgeous Granite Cheese",
+    package_size: "72 ml",
+    price: 237,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/9",
+    name: "Intelligent Granite Mouse",
+    package_size: "9 ml",
+    price: 674,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/9",
+    name: "Incredible Frozen Chair",
+    package_size: "64 ml",
+    price: 921,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/5",
+    name: "Licensed Metal Soap",
+    package_size: "60 mg",
+    price: 159,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/2",
+    name: "Gorgeous Plastic Soap",
+    package_size: "75 ml",
+    price: 536,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/9",
+    name: "Unbranded Wooden Sausages",
+    package_size: "4 mg",
+    price: 503,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/4",
+    name: "Unbranded Frozen Gloves",
+    package_size: "63 ml",
+    price: 311,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/9",
+    name: "Fantastic Rubber Pants",
+    package_size: "74 ml",
+    price: 250,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/transport/6",
+    name: "Handmade Rubber Ball",
+    package_size: "56 mg",
+    price: 808,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/6",
+    name: "Sleek Wooden Tuna",
+    package_size: "84 ml",
+    price: 375,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/food/5",
+    name: "Rustic Metal Mouse",
+    package_size: "19 ml",
+    price: 712,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/sports/3",
+    name: "Awesome Soft Sausages",
+    package_size: "37 mg",
+    price: 552,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/6",
+    name: "Sleek Cotton Sausages",
+    package_size: "43 ml",
+    price: 421,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/2",
+    name: "Gorgeous Concrete Pants",
+    package_size: "100 mg",
+    price: 997,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/sports/2",
+    name: "Intelligent Soft Computer",
+    package_size: "36 ml",
+    price: 210,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/6",
+    name: "Practical Metal Car",
+    package_size: "36 mg",
+    price: 321,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/8",
+    name: "Intelligent Concrete Cheese",
+    package_size: "94 mg",
+    price: 853,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/7",
+    name: "Refined Cotton Soap",
+    package_size: "23 ml",
+    price: 303,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/7",
+    name: "Tasty Cotton Bike",
+    package_size: "1 ml",
+    price: 850,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/4",
+    name: "Generic Frozen Hat",
+    package_size: "23 ml",
+    price: 229,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/6",
+    name: "Fantastic Concrete Fish",
+    package_size: "89 mg",
+    price: 811,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/6",
+    name: "Generic Rubber Table",
+    package_size: "44 mg",
+    price: 110,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/9",
+    name: "Generic Fresh Salad",
+    package_size: "93 mg",
+    price: 478,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/5",
+    name: "Awesome Fresh Bacon",
+    package_size: "39 mg",
+    price: 583,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/2",
+    name: "Rustic Frozen Hat",
+    package_size: "50 mg",
+    price: 362,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/people/3",
+    name: "Awesome Concrete Bike",
+    package_size: "42 ml",
+    price: 252,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/6",
+    name: "Intelligent Rubber Gloves",
+    package_size: "46 ml",
+    price: 927,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/9",
+    name: "Licensed Frozen Shoes",
+    package_size: "58 mg",
+    price: 377,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/6",
+    name: "Incredible Rubber Mouse",
+    package_size: "70 mg",
+    price: 543,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/6",
+    name: "Fantastic Steel Car",
+    package_size: "63 mg",
+    price: 187,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/sports/6",
+    name: "Handmade Rubber Soap",
+    package_size: "98 ml",
+    price: 589,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/1",
+    name: "Handmade Metal Bacon",
+    package_size: "34 mg",
+    price: 762,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/1",
+    name: "Sleek Wooden Shoes",
+    package_size: "83 ml",
+    price: 66,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/8",
+    name: "Handcrafted Rubber Chips",
+    package_size: "38 mg",
+    price: 487,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/cats/6",
+    name: "Awesome Fresh Car",
+    package_size: "83 ml",
+    price: 303,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/6",
+    name: "Small Cotton Fish",
+    package_size: "97 mg",
+    price: 268,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/9",
+    name: "Unbranded Wooden Hat",
+    package_size: "50 ml",
+    price: 481,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/1",
+    name: "Intelligent Steel Gloves",
+    package_size: "49 mg",
+    price: 506,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/7",
+    name: "Generic Fresh Soap",
+    package_size: "37 ml",
+    price: 148,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/6",
+    name: "Sleek Granite Sausages",
+    package_size: "69 ml",
+    price: 800,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/9",
+    name: "Licensed Soft Pants",
+    package_size: "7 mg",
+    price: 117,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/food/2",
+    name: "Tasty Frozen Bacon",
+    package_size: "45 ml",
+    price: 818,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/7",
+    name: "Incredible Frozen Table",
+    package_size: "93 ml",
+    price: 876,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/3",
+    name: "Licensed Steel Ball",
+    package_size: "60 mg",
+    price: 870,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/abstract/8",
+    name: "Intelligent Wooden Bacon",
+    package_size: "96 mg",
+    price: 516,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/food/7",
+    name: "Awesome Granite Pants",
+    package_size: "49 ml",
+    price: 193,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/animals/8",
+    name: "Practical Steel Chicken",
+    package_size: "48 mg",
+    price: 659,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nature/5",
+    name: "Gorgeous Frozen Bacon",
+    package_size: "50 ml",
+    price: 325,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/city/8",
+    name: "Practical Fresh Cheese",
+    package_size: "74 mg",
+    price: 248,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/technics/3",
+    name: "Rustic Wooden Chair",
+    package_size: "96 mg",
+    price: 840,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/business/6",
+    name: "Awesome Metal Bike",
+    package_size: "42 mg",
+    price: 606,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/6",
+    name: "Ergonomic Steel Car",
+    package_size: "45 mg",
+    price: 968,
+    availability: "not_available"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/nightlife/2",
+    name: "Refined Plastic Salad",
+    package_size: "99 mg",
+    price: 117,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/people/4",
+    name: "Handmade Concrete Pizza",
+    package_size: "23 ml",
+    price: 361,
+    availability: "in_stock"
+  },
+  {
+    img_url: "https://lorempixel.com/640/480/fashion/3",
+    name: "Incredible Wooden Salad",
+    package_size: "53 ml",
+    price: 647,
+    availability: "in_stock"
+  }
+];
+
+function render(searchValue) {
+  // todo create the html for the products which matches the searchValue:
+  // hint: use array-methods on the products-array and .join("") the result
+
+  const productsHtml = searchValue;
+
+  document.getElementById("search-result-container").innerHTML = productsHtml;
+}
+
+document.getElementById("update-button").onclick = () => {
+  const searchValue = document.getElementById("search-input").value;
+  render(searchValue);
+};
